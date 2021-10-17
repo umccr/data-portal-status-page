cfdbc0c9f26ec19a77c1aa7a7d7d47dbcdc80be3
William <Wintan@student.unimelb.edu.au>
2021-10-18 02:11:48 +1100

    Add Predeployment Stack

--- deploy/app.py
+++ deploy/app.py
@@ -5,25 +5,28 @@ from aws_cdk import (
     core as cdk,   
     aws_ssm as ssm
 )
+
+from stacks.predeploy import PredeploymentStack
 from pipelines.cdkpipeline import CdkPipelineStack
-
-account_id = os.environ.get('CDK_DEFAULT_ACCOUNT')
-aws_region = os.environ.get('CDK_DEFAULT_REGION')
-aws_env = {'account': account_id , 'region': aws_region}
-
  
 app = cdk.App()
-
-
 
 CdkPipelineStack(
   app,
   "DataPortalStatusPageCdkPipeline",
   stack_name = "cdkpipeline-data-portal-status-page",
-  env=aws_env,
   tags={
     "environment":"dev",
     "stack":"cdkpipeline-data-portal-status-page"
+  }
+)
+
+PredeploymentStack(
+  app,
+  "PredeploymentStack",
+  stack_name = "Predeployment-sscheck-front-end",
+  tags={
+    "stack":"cdkpipeline-sscheck-front-end"
   }
 )
 
