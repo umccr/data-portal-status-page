6a28f39ff5a5275732dca74ed57ce46635f5ac19
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-02-17 15:19:32 +1100

    Add Invalidate CDN cache

--- deploy/stacks/data_portal_status_page_stack.py
+++ deploy/stacks/data_portal_status_page_stack.py
@@ -1,12 +1,16 @@
+import os
 from aws_cdk import (
-    core as cdk,   
+    core as cdk,
     aws_s3 as s3,
     aws_cloudfront as cloudfront,
     aws_ssm as ssm,
     aws_route53 as route53,
     aws_route53_targets as route53t,
-    aws_certificatemanager as acm
+    aws_certificatemanager as acm,
+    aws_codebuild as codebuild,
+    aws_iam as iam
 )
+
 
 class DataPortalStatusPageStack(cdk.Stack):
 
@@ -44,9 +48,8 @@ class DataPortalStatusPageStack(cdk.Stack):
         cert_use1_arn = ssm.StringParameter.from_string_parameter_name(
             self,
             "CertUSE1ARN",
-            string_parameter_name ="/data_portal/status_page/ssl_certificate_arn"
+            string_parameter_name="/data_portal/status_page/ssl_certificate_arn"
         ).string_value
-
 
         cert_use1 = acm.Certificate.from_certificate_arn(
             self,
@@ -56,48 +59,50 @@ class DataPortalStatusPageStack(cdk.Stack):
 
         # Creating bucket for the build directory code
         client_bucket = s3.Bucket(
-            self, 
-            "data-portal-status-page-client-bucket", 
-            bucket_name = bucket_name,
-            auto_delete_objects = True,
-            removal_policy = cdk.RemovalPolicy.DESTROY,
-            website_index_document = "index.html",
-            website_error_document = "index.html",
-            block_public_access= s3.BlockPublicAccess.BLOCK_ALL
+            self,
+            "data-portal-status-page-client-bucket",
+            bucket_name=bucket_name,
+            auto_delete_objects=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            website_index_document="index.html",
+            website_error_document="index.html",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
         )
 
         # Create the Origin Access Identity (OAI)
         cloudfront_oai = cloudfront.OriginAccessIdentity(self, 'umccr-script-oai',
-            comment = "Created By CDK")
-        
+                                                         comment="Created By CDK")
+
         # create s3 configuration details
         s3_origin_source = cloudfront.S3OriginConfig(
-            s3_bucket_source = client_bucket,
-            origin_access_identity = cloudfront_oai                                
+            s3_bucket_source=client_bucket,
+            origin_access_identity=cloudfront_oai
         )
 
         # setup cloudfront config for s3
         source_config = cloudfront.SourceConfiguration(
-            s3_origin_source = s3_origin_source,
+            s3_origin_source=s3_origin_source,
             behaviors=[cloudfront.Behavior(is_default_behavior=True)]
         )
 
         # setup error pages redirection
         error_page_configuration = cloudfront.CfnDistribution.CustomErrorResponseProperty(
-            error_code = 403,
-            error_caching_min_ttl  = 60,
-            response_code = 200,
-            response_page_path = "/index.html"
+            error_code=403,
+            error_caching_min_ttl=60,
+            response_code=200,
+            response_page_path="/index.html"
         )
 
         # create the web distribution
-        data_portal_status_page_cloudfront = cloudfront.CloudFrontWebDistribution(self, "cloud_front_name",
+        data_portal_status_page_cloudfront = cloudfront.CloudFrontWebDistribution(
+            self,
+            "cloud_front_name",
             origin_configs=[source_config],
-            error_configurations = [error_page_configuration],
-            viewer_protocol_policy = cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            default_root_object = "index.html",
-            price_class = cloudfront.PriceClass.PRICE_CLASS_ALL,
-            enable_ip_v6 = False,
+            error_configurations=[error_page_configuration],
+            viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            default_root_object="index.html",
+            price_class=cloudfront.PriceClass.PRICE_CLASS_ALL,
+            enable_ip_v6=False,
             viewer_certificate=cloudfront.ViewerCertificate.from_acm_certificate(
                 certificate=cert_use1,
                 aliases=props["alias_domain_name"][app_stage],
@@ -117,3 +122,44 @@ class DataPortalStatusPageStack(cdk.Stack):
             record_name="status.data"
         )
 
+        # Adding codebuild for invalidate CDN cache for every s3 deployment
+        codebuild_build_image = codebuild.Project(
+            self,
+            "CodebuildProjectInvalidateStatsPageCDNCache",
+            source=codebuild.Source.git_hub(
+                owner="umccr",
+                repo="data-portal-status-page",
+                webhook=True,
+                webhook_filters=[
+                    codebuild.FilterGroup.in_event_of(codebuild.EventAction.PUSH).and_branch_is(
+                        props["branch_source"][app_stage]).and_file_path_is("src")
+                ]
+            ),
+            project_name="InvalidateDataPortalStatusPageCDNCache",
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_5_0
+            ),
+            build_spec=codebuild.BuildSpec.from_object({
+                "version": "0.2",
+                "phases": {
+                    "build": {
+                        "commands": [
+                            """aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "/*" """
+                        ]
+                    }
+                }
+            }),
+            environment_variables={
+                "DISTRIBUTION_ID": codebuild.BuildEnvironmentVariable(
+                    value=data_portal_status_page_cloudfront.distribution_id)
+            }
+        )
+
+        codebuild_build_image.add_to_role_policy(
+            iam.PolicyStatement(
+                resources=[
+                    f"arn:aws:cloudfront::{os.environ.get('CDK_DEFAULT_ACCOUNT')}:distribution/{data_portal_status_page_cloudfront.distribution_id}"
+                ],
+                actions=["cloudfront:CreateInvalidation"]
+            )
+        )
