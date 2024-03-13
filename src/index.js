94c70a708739ace57eba23bfd5b1b02cdaf43a13
raylrui <ray.liu@umccr.org>
2024-03-13 11:12:41 +1100

    fix warning and conflict after upgrade to react 18 and mui 5.15

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -31,13 +31,12 @@ Amplify.configure({
   },
 });
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
